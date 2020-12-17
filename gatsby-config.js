5864e23d0bd478d7e3ff4b7d00df5543744e1233
ChrisAchinga <achinga.chris@gmail.com>
2020-12-17 10:38:59 +0300

    update config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,12 +1,12 @@
 module.exports = {
   siteMetadata: {
     // edit below
-    title: `Gatsby Starter Personal Blog`,
+    title: `The Developer Times`,
     author: `Gatsby`,
-    description: `A starter personal blog with styled components, dark mode, and Netlify CMS.`,
-    siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
+    description: `Best and Most Worded Technical Articles For Developers`,
+    siteUrl: `https://blogzone.netlify.app/`,
     social: {
-      twitter: `gatsbyjs`,
+      twitter: `twitter`,
     },
   },
   plugins: [
--- src/pages/index.js
+++ src/pages/index.js
@@ -7,7 +7,7 @@ import Button from "../components/button"
 
 class IndexPage extends React.Component {
   render() {
-    const siteTitle = "Gatsby Starter Personal Website"
+    const siteTitle = "The Developer Times"
 
     return (
       <Layout location={this.props.location} title={siteTitle}>
